5d29217029a4a5cbe5a004818547a9aa1ebbbeb9
Sebastian Castillo <tantrum5535@gmail.com>
2024-04-28 20:30:01 -0400

    correccion ejercicio1

--- javascript/ejercicio1.js
+++ javascript/ejercicio1.js
@@ -1,5 +1,5 @@
 // Escribir una función llamada contrasenaValida que reciba un string y retorne true si el string es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9". De lo contrario debe retornar false.
-texto = "eoZiugBf&g9"
+const texto = "eoZiugBf&g9"
 
 function contrasenaValida (texto){
     return texto == "2Fj(jjbFsuj" || texto == "eoZiugBf&g9"
